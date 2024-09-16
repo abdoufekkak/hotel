e256fb59dd3208d8a45e6f0fbb8737f7fd8dfe06
abdoufekkak <abdou.fekkak66@gmail.com>
2024-09-16 16:26:00 +0100

    WER

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 // Importer Express avec require
 const express = require('express');
-
+const cors=require('cors')
 // Créer une instance d'application
 const app = express();
 
 // Définir le port sur lequel le serveur écoutera
 const port = 3000;
+const allowedDomains = ['https://protelhere.com'];
 
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedDomains.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['OPTIONS', 'GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token', 'X-Requested-With', 'Accept', 'Accept-Version'],
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions))
 // Créer une route simple
 app.get('/', (req, res) => {
   res.send('Bienvenue sur mon application Node.js avec Express!');
